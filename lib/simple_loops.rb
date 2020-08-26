681796c7730494d72aadada97444ccc69d46b54f
Tyler Wallace <tywallace2017@gmail.com>
2020-08-26 21:57:23 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,7 +2,7 @@
 def loop_message_five_times(message)
   count = 0 
   message = ["Hi", "hello", "wiwi", "hola", "ohana"]
-  while count < (message) do
+  while count(message) do
     puts message(counter)
     count += 5
   end
