3bde32ba2d9c2c30cfcf8f8bdcb1a11e0b984e69
Tyler Wallace <tywallace2017@gmail.com>
2020-08-26 22:02:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,3 +6,11 @@ def loop_message_five_times(message)
     count += 1 
   end
 end 
+
+def loop_message_n_times(message)
+  count = 0 
+  while count < 9 do 
+    puts message 
+    count += 1 
+  end 
+end
